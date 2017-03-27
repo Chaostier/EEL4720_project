f8b90871c28be7d39fc7af80c03e7157760ad72f
Evan Bishop <evanbishop9@gmail.com>
2017-03-27 17:31:35 -0400

    Initial commit

---
+++ ip_repo/neural_net_1.0/src/neuron.vhd
@@ -0,0 +1,21 @@
+--Evan Bishop and Amber Ransford
+--EEL4720 Project - neuron functional unit
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity neuron is
+
+generic(WIDTH: positive);
+port(
+
+  );
+
+end entity;
+
+architecture BHV of adder is
+
+begin
+
+end BHV;
